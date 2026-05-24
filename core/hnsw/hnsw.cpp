81cf57eb4609e640357fe70801c1e46e5e867652
Akaash Dhayalan <akaashdhayalan243@gmail.com>
2026-05-24 09:27:20 -0400

    Defined HNSW class

--- core/hnsw/hnsw.cpp
+++ core/hnsw/hnsw.cpp
@@ -6,6 +6,22 @@ struct Node
 {
     int id;
     vector<float> data;
-    vector<std::vector<int>> neighbors;
+    vector<vector<int>> neighbors;
     bool deleted = false;
+};
+
+class HNSW
+{
+    int M;
+    int efConstruction;
+    int maxLayer;
+    int entryPoint;
+    vector<Node> nodes;
+
+public:
+    HNSW(int M, int efConstruction);
+
+    void addNode(int id, const vector<float> &data);
+    void deleteNode(int id);
+    vector<int> searchANN(const vector<float> &query, int k, int ef);
 };
