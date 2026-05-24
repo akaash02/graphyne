b570c31f3a6a3b6725c972e5b5f65b1fdfd6d297
Akaash Dhayalan <akaashdhayalan243@gmail.com>
2026-05-24 12:17:48 -0400

    Added greedyDescent function to hnsw

--- core/hnsw/hnsw.cpp
+++ core/hnsw/hnsw.cpp
@@ -15,8 +15,50 @@ class HNSW
     int M;
     int efConstruction;
     int maxLayer;
-    int entryPoint;
+    Node entryPoint;
     vector<Node> nodes;
+
+    float dist(const vector<float> &a, const vector<float> &b)
+    {
+        float sum = 0;
+        for (size_t i = 0; i < a.size(); ++i)
+        {
+            sum += (a[i] - b[i]) * (a[i] - b[i]);
+        }
+
+        return sum;
+    }
+
+    int greedyDescend(const vector<float> &query, int targetLayer)
+    {
+        int currentNodeId = entryPoint.id;
+        int currentLayer = maxLayer;
+        float bestDist = this->dist(query, entryPoint.data);
+
+        while (currentLayer > targetLayer)
+        {
+            bool changed = false;
+
+            for (int neighborId : nodes[currentNodeId].neighbors[currentLayer])
+            {
+                float d = this->dist(query, nodes[neighborId].data);
+
+                if (d < bestDist)
+                {
+                    bestDist = d;
+                    currentNodeId = neighborId;
+                    changed = true;
+                }
+            }
+
+            if (!changed)
+            {
+                currentLayer--;
+            }
+        }
+
+        return currentNodeId;
+    }
 
 public:
     HNSW(int M, int efConstruction);
